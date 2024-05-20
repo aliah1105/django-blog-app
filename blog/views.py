a25ffcf1bbb33daa14cc6ba64b9e7979a1121e12
aliah1105 <aliahadi.f1105@gmail.com>
2024-05-20 20:29:19 +0330

    return articles from database in view

--- blog/views.py
+++ blog/views.py
@@ -1,44 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from .models import Article
 
 # Create your views here.
 
 def home(request):
     context = {
-        "articles": [
-            {
-                "title": "first article",
-                "description": "first article description"
-            },
-            {
-                "title": "second article",
-                "description": "second article description"
-            },
-            {
-                "title": "third article",
-                "description": "third article description"
-            }
-        ]
+        "articles": Article.objects.filter(status="p").order_by('-publish')
     }
     return render(request, 'blog/home.html', context)
-
-
-def api(request):
-    data = {
-        "1": {
-            "id": 1,
-            "title": "first article",
-            "slug": "first-article",
-        },
-        "2": {
-            "id": 2,
-            "title": "second article",
-            "slug": "second-article",
-        },
-        "3": {
-            "id": 3,
-            "title": "third article",
-            "slug": "third-article",
-        }
-    }
-    return JsonResponse(data)
