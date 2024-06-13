396b138b7b2c16f4d6ded298aa472faa25eb41b9
aliah1105 <aliahadi.f1105@gmail.com>
2024-06-13 20:11:34 +0330

    add persian language to admin panel

--- blog/models.py
+++ blog/models.py
@@ -10,14 +10,18 @@ class Article(models.Model):
         ('p', 'Published'),
     )
 
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to="images")
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
-    publish = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=200, verbose_name='عنوان مقاله')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='آدرس مقاله')
+    description = models.TextField(verbose_name='توضیحات')
+    thumbnail = models.ImageField(upload_to="images", verbose_name='تصویر')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name='وضعیت')
+    publish = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقالات"
 
     def __str__(self) -> str:
         return self.title
