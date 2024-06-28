5d48b3aefc805ee13c707752ff5e6075aa6d1ae5
aliah1105 <aliahadi.f1105@gmail.com>
2024-06-28 10:56:12 +0330

    add category model and use in article, admin panel

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,21 @@ from extentions.utils import jalali_convertor
 
 # Create your models here.
 
+class Category(models.Model):
+    title = models.CharField(max_length=200, verbose_name='عنوان دسته‌بندی')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='آدرس دسته‌بندی')
+    status = models.BooleanField(default=True, verbose_name="آیا نمایش داده شود؟")
+    position = models.IntegerField(verbose_name="پوزیشن")
+
+    class Meta:
+        verbose_name = "دسته‌بندی"
+        verbose_name_plural = "دسته‌بندی‌ها"
+        ordering = ['position']
+
+    def __str__(self) -> str:
+        return self.title
+
+
 class Article(models.Model):
 
     STATUS_CHOICES = (
@@ -13,6 +28,7 @@ class Article(models.Model):
 
     title = models.CharField(max_length=200, verbose_name='عنوان مقاله')
     slug = models.SlugField(max_length=100, unique=True, verbose_name='آدرس مقاله')
+    category = models.ManyToManyField(Category, verbose_name='دسته‌بندی')
     description = models.TextField(verbose_name='توضیحات')
     thumbnail = models.ImageField(upload_to="images", verbose_name='تصویر')
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name='وضعیت')
